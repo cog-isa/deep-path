022b0793602c377f8274a82518dc53a900fa0112
Roman <windj007@gmail.com>
2016-10-18 16:21:38 +0300

    plot smoothing, increase size

--- dlpf/plot_utils.py
+++ dlpf/plot_utils.py
@@ -12,7 +12,7 @@ STYLES_GEN = itertools.cycle([''.join(s) for s in itertools.product(LINES, MARKE
 
 def basic_plot(title_data_tuples, out_file = None):
     fig, ax = plt.subplots()
-    fig.set_size_inches((10, 6))
+    fig.set_size_inches((16, 10))
     for (name, x, y), style in itertools.izip(title_data_tuples,
                                               STYLES_GEN):
         ax.plot(x, y, style, label = name)
@@ -22,11 +22,22 @@ def basic_plot(title_data_tuples, out_file = None):
     return fig, ax
 
 
-def basic_plot_from_df(df, out_file = None):
-    if df.shape[1] > 0:
+def basic_plot_from_df(df, out_file = None, need_get_dummies = True):
+    if need_get_dummies and df.shape[1] > 0:
         df = pandas.get_dummies(df)
     return basic_plot(((col, df.index, df[col].values) for col in df.columns),
                       out_file = out_file)
+
+
+def basic_plot_from_df_rolling_mean(df, window = None, smooth_factor = 20.0, out_file = None):
+    if df.shape[1] > 0:
+        df = pandas.get_dummies(df)
+    if window is None:
+        window = min(int(float(df.shape[0]) / smooth_factor), 10)
+    df = pandas.rolling_mean(df, window)
+    return basic_plot_from_df(df,
+                              out_file = out_file,
+                              need_get_dummies = False)
 
 
 def basic_plot_via_df(raw_data, out_file = None):
