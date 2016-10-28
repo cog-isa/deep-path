2d831a0e05bfceca4522476d7c95d1a0fb11a5a7
Roman <windj007@gmail.com>
2016-10-28 16:24:45 +0300

    convolutional agent seems to work somehow (worse than simple)

--- dlpf/agents/our_dqn.py
+++ dlpf/agents/our_dqn.py
@@ -1,7 +1,11 @@
-import keras
-from keras.layers import Dense, Flatten, Dropout, Reshape, Convolution2D
+import keras, logging
+import keras.backend as K
+from keras.layers import Dense, Flatten, Dropout, Reshape, Convolution2D, MaxPooling2D, AveragePooling2D
 
 from .base import BaseKerasAgent
+
+
+logger = logging.getLogger()
 
 
 class OneLayerAgent(BaseKerasAgent):
@@ -36,27 +40,47 @@ class ConvAndDenseAgent(BaseKerasAgent):
                  hidden_dropouts = [0.2],
                  conv_cores = [10],
                  conv_core_sizes = [4],
+                 conv_strides = [2],
                  conv_activations = ['relu'],
                  conv_dropouts = [0.2],
+                 conv_pooling = ['max'],
                  *args, **kwargs):
         self.hidden_sizes = hidden_sizes
         self.hidden_activations = hidden_activations
         self.hidden_dropouts = hidden_dropouts
         self.conv_cores = conv_cores
         self.conv_core_sizes = conv_core_sizes
+        self.conv_strides = conv_strides
         self.conv_activations = conv_activations
         self.conv_dropouts = conv_dropouts
-        super(OneConvPlusTwoLayerAgent, self).__init__(*args, **kwargs)
+        self.conv_pooling = conv_pooling
+        super(ConvAndDenseAgent, self).__init__(*args, **kwargs)
 
     def _build_inner_model(self, input_layer):
         h = input_layer
 
-        for cores, core_size, act, dropout in zip(self.conv_cores,
-                                                  self.conv_core_sizes,
-                                                  self.conv_activations,
-                                                  self.conv_dropouts):
-            h = Convolution2D(cores, core_size, core_size, activation = act)(input_layer)
+        if len(self.input_shape) == 2:
+            layer_ordering = K.image_dim_ordering()
+            if layer_ordering == 'th': # theano is (layers, rows, cols)
+                reshape_input_to = (1,) + self.input_shape
+            else:
+                reshape_input_to = self.input_shape + (1,) # tensorflow is (rows, cols, layers)
+            h = Reshape(reshape_input_to)(h)
+
+        for cores, core_size, stride, act, dropout, pool in zip(self.conv_cores,
+                                                                self.conv_core_sizes,
+                                                                self.conv_strides,
+                                                                self.conv_activations,
+                                                                self.conv_dropouts,
+                                                                self.conv_pooling):
+            h = Convolution2D(cores,
+                              core_size,
+                              core_size,
+                              activation = act,
+                              subsample = (stride, stride))(h)
             h = Dropout(dropout)(h)
+            if pool in ('max', 'min'):
+                h = (MaxPooling2D if pool == 'max' else AveragePooling2D)()(h)
         h = Flatten()(h)
 
         for size, act, dropout in zip(self.hidden_sizes,
