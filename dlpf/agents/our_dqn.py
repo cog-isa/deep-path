b4b5d5995fa4480daedddb1c1bf50f107d2c88cd
Roman <windj007@gmail.com>
2016-11-07 11:05:53 +0300

    add batchnorm and some configs that use it

--- dlpf/agents/our_dqn.py
+++ dlpf/agents/our_dqn.py
@@ -1,6 +1,8 @@
-import keras, logging
+import keras, logging, itertools
 import keras.backend as K
-from keras.layers import Dense, Flatten, Dropout, Reshape, Convolution2D, MaxPooling2D, AveragePooling2D
+from keras.layers import Dense, Flatten, Dropout, Reshape, \
+    Convolution2D, MaxPooling2D, AveragePooling2D, \
+    BatchNormalization, Activation
 
 from .base import BaseKerasAgent
 
@@ -38,56 +40,69 @@ class ConvAndDenseAgent(BaseKerasAgent):
                  hidden_sizes = [10],
                  hidden_activations = ['relu'],
                  hidden_dropouts = [0.2],
+                 hidden_batchnorm = [False],
                  conv_cores = [10],
                  conv_core_sizes = [4],
                  conv_strides = [2],
                  conv_activations = ['relu'],
                  conv_dropouts = [0.2],
                  conv_pooling = ['max'],
+                 conv_batchnorm = [False],
                  *args, **kwargs):
         self.hidden_sizes = hidden_sizes
         self.hidden_activations = hidden_activations
         self.hidden_dropouts = hidden_dropouts
+        self.hidden_batchnorm = hidden_batchnorm
         self.conv_cores = conv_cores
         self.conv_core_sizes = conv_core_sizes
         self.conv_strides = conv_strides
         self.conv_activations = conv_activations
         self.conv_dropouts = conv_dropouts
         self.conv_pooling = conv_pooling
+        self.conv_batchnorm = conv_batchnorm
         super(ConvAndDenseAgent, self).__init__(*args, **kwargs)
 
     def _build_inner_model(self, input_layer):
         h = input_layer
 
+        is_theano = K.image_dim_ordering() == 'th'
         if len(self.input_shape) == 2:
-            layer_ordering = K.image_dim_ordering()
-            if layer_ordering == 'th': # theano is (layers, rows, cols)
+            if is_theano: # theano is (layers, rows, cols)
                 reshape_input_to = (1,) + self.input_shape
             else:
                 reshape_input_to = self.input_shape + (1,) # tensorflow is (rows, cols, layers)
             h = Reshape(reshape_input_to)(h)
 
-        for cores, core_size, stride, act, dropout, pool in zip(self.conv_cores,
-                                                                self.conv_core_sizes,
-                                                                self.conv_strides,
-                                                                self.conv_activations,
-                                                                self.conv_dropouts,
-                                                                self.conv_pooling):
+        bn_axis = 0 if is_theano else 2
+        for cores, core_size, stride, act, dropout, pool, bn in itertools.izip_longest(self.conv_cores,
+                                                                                       self.conv_core_sizes,
+                                                                                       self.conv_strides,
+                                                                                       self.conv_activations,
+                                                                                       self.conv_dropouts,
+                                                                                       self.conv_pooling,
+                                                                                       self.conv_batchnorm):
             h = Convolution2D(cores,
                               core_size,
                               core_size,
-                              activation = act,
                               subsample = (stride, stride))(h)
-            h = Dropout(dropout)(h)
+            if not dropout is None and 0 < dropout < 1:
+                h = Dropout(dropout)(h)
+            if bn:
+                h = BatchNormalization(axis = bn_axis)(h)
             if pool in ('max', 'min'):
                 h = (MaxPooling2D if pool == 'max' else AveragePooling2D)()(h)
+            h = Activation(act)(h)
         h = Flatten()(h)
 
-        for size, act, dropout in zip(self.hidden_sizes,
-                                      self.hidden_activations,
-                                      self.hidden_dropouts):
-            h = Dense(size,
-                      activation = act)(h)
-            h = Dropout(dropout)(h)
+        for size, act, dropout, bn in itertools.izip_longest(self.hidden_sizes,
+                                                             self.hidden_activations,
+                                                             self.hidden_dropouts,
+                                                             self.hidden_batchnorm):
+            h = Dense(size)(h)
+            if not dropout is None and 0 < dropout < 1:
+                h = Dropout(dropout)(h)
+            if bn:
+                h = BatchNormalization()(h)
+            h = Activation(act)(h)
 
         return h
